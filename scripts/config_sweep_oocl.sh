fc16c6805a234575b304a2846f63a1b5a9c376dd
Luan Fletcher <fletchel@tcd.ie>
2024-06-02 20:23:26 +0200

    minor fix to oocl sweep script

--- scripts/config_sweep_oocl.sh
+++ scripts/config_sweep_oocl.sh
@@ -4,7 +4,7 @@ model_path="${1:-None}"
 
 project_name="config_sweep_oocl"
 pretrain_runs=(1 2 3)
-oocl_run=(1)
+oocl_runs=(1)
 n_layers_list=(2 4 6)
 d_models=(128 256 512)
 attn_only_list=(True False)
@@ -21,8 +21,8 @@ do
     do
     for attn_only in "${attn_only_list[@]}"
     do
-        saved_model_name="pretraining_d_model_${d_model}_n_layers_${n_layers}_attnonly_${attn_only}_run_${pretrain_run}"
-        wandb_name="oocl_d_model_${d_model}_n_layers_${n_layers}_attnonly_${attn_only}_pretrainrun_${pretrain_run}_ooclrun_${oocl_run}"
+        saved_model_name="pretraining_d_model_${d_model}_n_layers_${n_layer}_attnonly_${attn_only}_run_${pretrain_run}"
+        wandb_name="oocl_d_model_${d_model}_n_layers_${n_layer}_attnonly_${attn_only}_pretrainrun_${pretrain_run}_ooclrun_${oocl_run}"
         echo "Running oocl for: project_name=$project_name, seed=$seed, n_layers=$n_layer, d_model=$d_model, attn_only=$attn_only, pretrain_run=$pretrain_run, oocl_run=$oocl_run"
         python -u ../oocl.py --wandb_name wandb_name --saved_model_name $saved_model_name --project_name $project_name --n_layers $n_layer --d_model $d_model --attn_only $attn_only --model_path $model_path
     done
